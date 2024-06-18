ffb7b67cbd80a0f4354d55b3a394e6619a95ce45
Tellis <tellis.george@gmail.com>
2024-06-18 16:17:52 +0100

    updated java code with neighbour

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -14,6 +14,7 @@ public class App
         System.out.println(sayHelloToSomeone("Charlie"));
         System.out.println(sayHelloToSomeone("Zena"));
         System.out.println(sayHelloToSomeone("TELLIS"));
+        System.out.println(sayHelloToSomeone("STEVE"));
         System.out.println(sayGoodbye());
     }
 
